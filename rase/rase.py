81b840e575fbb43d8d2f6e1ada64211decdd9919
Karel Břinda <karel.brinda@gmail.com>
2019-01-31 16:36:45 -0500

    Fix piping from prophyle to rase_predict

--- rase/rase.py
+++ rase/rase.py
@@ -1,5 +1,4 @@
 #! /usr/bin/env python3
-
 """rase
 
 Author:  Karel Brinda <kbrinda@hsph.harvard.edu>
@@ -30,22 +29,41 @@ def run(*args):
 def rase(db, reads):
     db_dir = os.path.dirname(db)
     db_pref = os.path.basename(db)
-    run(*['cd', db_dir, '&&', 'prophyle', 'decompress', db_pref+".tar.gz"])
-    run(*['prophyle', 'classify', db, reads,
-          '|', script_dir+'/../scripts/rase_predict.py', db+"/tree.nw", db+'.tsv', '-', #'-p', prediction/{wildcards.pref}__{wildcards.index}/
-          ])
-          #> {params.tsv1}
+    run(*['cd', db_dir, '&&', 'prophyle', 'decompress', db_pref + ".tar.gz"])
+
+    args_classify = ['prophyle', 'classify', db, reads]
+
+    args_predict = [
+        script_dir + '/../scripts/rase_predict.py',
+        '-t',
+        'clock',
+        '-i',
+        '1',
+        db + "/tree.nw",
+        db + '.tsv',
+        '-',  #'-p', prediction/{wildcards.pref}__{wildcards.index}/
+    ]
+
+    process_classify = subprocess.Popen(args_classify, stdout=subprocess.PIPE, shell=False)
+    process_predict = subprocess.Popen(
+        args_predict,
+        stdin=process_classify.stdout,
+        #stdout=subprocess.PIPE,
+        shell=False
+    )
+
+    process_classify.stdout.close()
+    process_predict.communicate()  # return ... [0]
+
+    #> {params.tsv1}
+
 
 def main():
     parser = argparse.ArgumentParser(description="")
 
-    parser.add_argument(
-        'rase_db'
-    )
+    parser.add_argument('rase_db')
 
-    parser.add_argument(
-        'reads',
-    )
+    parser.add_argument('reads', )
 
     args = parser.parse_args()
     rase(db=args.rase_db, reads=args.reads)
