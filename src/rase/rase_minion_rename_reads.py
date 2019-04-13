6a748ce8281fbfa87fa81bf2a9b3b9693723a73d
Karel Břinda <karel.brinda@gmail.com>
2019-04-13 11:39:45 -0400

    Add support for minion fasta files

--- src/rase/rase_minion_rename_reads.py
+++ src/rase/rase_minion_rename_reads.py
@@ -22,8 +22,7 @@ import sys
 #    start_time=(\d{4}-\d{2}-\d{2})T(\d{2}:\d{2}:\d{2})''', flags=re.X)
 
 re_minion = re.compile(
-    r'''^[>@]
-        ([0-9a-f\-]+) \s
+    r'''^
         runid=([0-9a-f]+) \s
         read=(\d+) \s
         ch=(\d+) \s
@@ -63,6 +62,47 @@ def timestamp_from_datetime(date, time):
     return ts
 
 
+def readfq(fp):
+    """
+    Parse FASTA/FASTQ.
+
+    Original version: https://github.com/lh3/readfq/blob/master/readfq.py
+    Changes: reads also comments, returns (name, comment, seq, quals)
+    """
+    last = None  # this is a buffer keeping the last unprocessed line
+    while True:  # mimic closure; is it a bad idea?
+        if not last:  # the first record or a record following a fastq
+            for l in fp:  # search for the start of the next record
+                if l[0] in '>@':  # fasta/q header line
+                    last = l[:-1]  # save this line
+                    break
+        if not last: break
+        name, _, comment = last[1:].partition(" ")
+        seqs = []
+        last = None
+        for l in fp:  # read the sequence
+            if l[0] in '@+>':
+                last = l[:-1]
+                break
+            seqs.append(l[:-1])
+        if not last or last[0] != '+':  # this is a fasta record
+            yield name, comment, ''.join(seqs), None  # yield a fasta record
+            if not last: break
+        else:  # this is a fastq record
+            seq, leng, seqs = ''.join(seqs), 0, []
+            for l in fp:  # read the quality
+                seqs.append(l[:-1])
+                leng += len(l) - 1
+                if leng >= len(seq):  # have read enough quality
+                    last = None
+                    yield name, comment, seq, ''.join(seqs)
+                    # yield a fastq record
+                    break
+            if last:  # reach EOF before reading enough quality
+                yield name, comment, seq, None  # yield a fasta record instead
+                break
+
+
 def transf(fq):
     d = {}
 
@@ -72,6 +112,16 @@ def transf(fq):
         open_fn = open
 
     with open_fn(fq) as f:
+        for name, comment, seq, qual in readfq(f):
+            #print(comment, file=sys.stderr)
+            uuid = name
+            runid, read, ch, date, time = parse_header_line(comment)
+            ts = timestamp_from_datetime(date, time)
+            print(">" + "{}_EX{}_RD{}_CH{}".format(ts, runid[:9], uuid.partition("-")[0], ch))
+            print(seq)
+
+        return
+
         for i, x in enumerate(f):
             if i % 4 == 0:
                 uuid, runid, read, ch, date, time = parse_header_line(x)
