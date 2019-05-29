5678e5d616d38575365b7a8203b4b910f62d54e3
Karel Břinda <karel.brinda@gmail.com>
2019-05-29 17:53:51 -0400

    Add comments

--- src/rase/rase_prediction_add_flags.py
+++ src/rase/rase_prediction_add_flags.py
@@ -17,13 +17,24 @@ import csv
 import re
 
 
+def load_tsv_dl(tsv_fn):
+    """Load TSV as a list of dictionaries (1 dict per 1 line).
+    """
+    with open(tsv_fn) as f:
+        tr = [x for x in csv.DictReader(f, delimiter="\t")]
+    return tr
+
+
 def get_cols(tsv_dl):
+    """Get columns from a list of dictionaries.
+    """
     d = tsv_dl[0]
     return [x for x in d.keys()]
 
 
 def extract_flag_cols(cols):
-    # regular expressions for categories to be flagged
+    """Extract columns for which flags are to be computed.
+    """
     res_flagging = [
         re.compile(r"^bm$"),
         re.compile(r"^serotype$"),
@@ -41,14 +52,11 @@ def extract_flag_cols(cols):
     return flag_cols
 
 
-def load_tsv_dl(tsv_fn):
-    with open(tsv_fn) as f:
-        tr = [x for x in csv.DictReader(f, delimiter="\t")]
-    return tr
-
-
 def get_stabilization_point(tsv_dl, key):
-    tsv_dl_ = [collections.defaultdict(lambda: "")] + tsv_dl
+    """Find points of stabilization.
+    """
+    tsv_dl_ = [collections.defaultdict(lambda: "")
+               ] + tsv_dl  # probably to support empty files?
     final_value = tsv_dl_[-1][key]
     for i in range(len(tsv_dl_) - 1, -1, -1):
         if tsv_dl_[i][key] != final_value:
@@ -56,7 +64,26 @@ def get_stabilization_point(tsv_dl, key):
     return i
 
 
+def add_flags_line():
+    for k in flag_cols:
+        if i == stabilization_points[k]:
+            flags.append('S:{}'.format(k))
+            assert rec[k] != prev_rec[k]
+        if rec[k] != prev_rec[k]:
+            if rec[k] == last_rec[k]:
+                # detected
+                flags.append('D:{}'.format(k))
+            elif prev_rec[k] == last_rec[k]:
+                # lost successfuly detected
+                flags.append('L:{}'.format(k))
+    flags.sort()
+    flags_str = str(flags).replace("'", "")
+    print(*rec.values(), flags_str, sep="\t")
+
+
 def add_flags(tsv_fn):
+    """Read RASE prediction output and add flags.
+    """
     tsv_dl = load_tsv_dl(tsv_fn)
     cols = get_cols(tsv_dl)
     flag_cols = extract_flag_cols(cols)
